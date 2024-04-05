7bf8a574f8601232565dcd86dee7329caa4d3a77
tukaramgoud <tukaramgoud2804.com>
2024-04-05 17:58:08 +0000

    adding some code

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,1 +1,17 @@
 // Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Appointments extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="display-container">
+          <h1>hello world</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
