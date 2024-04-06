c39b4e78704ba6efd7b77200bac2d4732a5cfdbb
tukaramgoud <tukaramgoud2804.com>
2024-04-06 07:42:57 +0000

    adding coding files

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,22 @@
 // Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {itemDetails} = props
+  return (
+    <div className="AppointmentItem-container">
+      <div className="stared-isTrue">
+        <p className="mini-para">Dentist</p>
+        <button type="button" className="button-liked">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
+            alt="like"
+          />
+        </button>
+      </div>
+      <p className="mini-para-2">Date : 20 july, 2024, Saturday</p>
+    </div>
+  )
+}
+
+export default AppointmentItem
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,13 +1,74 @@
 // Write your code here
 import {Component} from 'react'
+import AppointmentItem from '../AppointmentItem'
 import './index.css'
 
 class Appointments extends Component {
+  state = {name: '', date: ''}
+
+  formSubmitted = event => {
+    event.preventDefault()
+    const {name, date} = this.state
+    console.log(name, date)
+  }
+
+  titleChanged = event => {
+    this.setState({name: event.target.value})
+  }
+
+  dateChanged = event => {
+    this.setState({date: event.target.value})
+  }
+
   render() {
+    const {name, date} = this.state
+
     return (
       <div className="main-container">
         <div className="display-container">
-          <h1>hello world</h1>
+          <div className="user-input-image-container">
+            <form className="form-container" onSubmit={this.formSubmitted}>
+              <h1 className="second-heading">Add Appointment</h1>
+              <label htmlFor="userInput" className="title-label">
+                Title
+              </label>
+              <input
+                id="userInput"
+                className="input-styling"
+                onChange={this.titleChanged}
+                value={name}
+                placeholder="Title"
+              />
+
+              <label htmlFor="userDate" className="title-label">
+                Date
+              </label>
+              <input
+                type="date"
+                id="userDate"
+                className="input-styling"
+                onChange={this.dateChanged}
+                value={date}
+              />
+              <button type="submit" className="button">
+                Add
+              </button>
+            </form>
+            <div className="image-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+                alt="appointments"
+              />
+            </div>
+          </div>
+          <hr className="line" />
+          <div className="filter-container">
+            <h1 className="second-heading">Appointments</h1>
+            <button type="button" className="stared-button">
+              Starred
+            </button>
+          </div>
+          <AppointmentItem />
         </div>
       </div>
     )
