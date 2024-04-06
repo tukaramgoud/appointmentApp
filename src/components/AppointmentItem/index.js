c4809dc75652104218ce34af29dadc46df4cb2e6
tukaramgoud <tukaramgoud2804.com>
2024-04-06 08:49:03 +0000

    finalizing the code

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -13,19 +13,20 @@ const AppointmentItem = props => {
     ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
   return (
-    <div className="AppointmentItem-container">
+    <li className="AppointmentItem-container">
       <div className="stared-isTrue">
         <p className="mini-para">{name}</p>
         <button
           type="button"
           className="button-liked"
           onClick={likedButtonClicked}
+          data-testid="star"
         >
-          <img src={imageUrl} alt="like" />
+          <img src={imageUrl} alt="star" />
         </button>
       </div>
       <p className="mini-para-2">{date}</p>
-    </div>
+    </li>
   )
 }
 
